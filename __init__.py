81308dc9a019c7339665e976d64324a864c838a3
Josh Dukes <jdukes@securityinnovation.com>
2012-06-20 15:53:35 -0700

    started unit testing module, also added start of utils for future dev

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from . import *
+
+__all__ = ["har", "utils"]
--- unit_test.py
+++ unit_test.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
+"""This module sets up unit tests for harpy.
 
+"""
+
+import unittest
+import har
