474e055cc4c41c0bdb15bf3368e2b6c7f2d50d27
Josh Dukes <jdukes@securityinnovation.com>
2012-06-21 12:25:29 -0700

    used pythoscope to generate base unit tests rather than doing so manually

---
+++ tests/test_har.py
@@ -0,0 +1,302 @@
+import unittest
+
+class TestHarEncoder(unittest.TestCase):
+    def test_default(self):
+        # har_encoder = HarEncoder()
+        # self.assertEqual(expected, har_encoder.default(obj))
+        assert False # TODO: implement your test here
+
+class test__MetaHar(unittest.TestCase):
+    def test___contains__(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.__contains__(obj))
+        assert False # TODO: implement your test here
+
+    def test___init__(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        assert False # TODO: implement your test here
+
+    def test___iter__(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.__iter__())
+        assert False # TODO: implement your test here
+
+    def test___repr__(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.__repr__())
+        assert False # TODO: implement your test here
+
+    def test___str__(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.__str__())
+        assert False # TODO: implement your test here
+
+    def test_from_dict(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.from_dict(json_dict))
+        assert False # TODO: implement your test here
+
+    def test_from_json(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.from_json(json_data))
+        assert False # TODO: implement your test here
+
+    def test_get_children(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.get_children())
+        assert False # TODO: implement your test here
+
+    def test_replace(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.replace(**kwarg))
+        assert False # TODO: implement your test here
+
+    def test_set_defaults(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.set_defaults())
+        assert False # TODO: implement your test here
+
+    def test_to_json(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.to_json())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # __meta_har = _MetaHar(init_from, parent, defaults)
+        # self.assertEqual(expected, __meta_har.validate())
+        assert False # TODO: implement your test here
+
+class test__KeyValueHar(unittest.TestCase):
+    def test___eq__(self):
+        # __key_value_har = _KeyValueHar()
+        # self.assertEqual(expected, __key_value_har.__eq__(other))
+        assert False # TODO: implement your test here
+
+    def test___repr__(self):
+        # __key_value_har = _KeyValueHar()
+        # self.assertEqual(expected, __key_value_har.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # __key_value_har = _KeyValueHar()
+        # self.assertEqual(expected, __key_value_har.validate())
+        assert False # TODO: implement your test here
+
+class TestHarContainer(unittest.TestCase):
+    def test___repr__(self):
+        # har_container = HarContainer()
+        # self.assertEqual(expected, har_container.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # har_container = HarContainer()
+        # self.assertEqual(expected, har_container.validate())
+        assert False # TODO: implement your test here
+
+class TestLog(unittest.TestCase):
+    def test___repr__(self):
+        # log = Log()
+        # self.assertEqual(expected, log.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # log = Log()
+        # self.assertEqual(expected, log.validate())
+        assert False # TODO: implement your test here
+
+class TestCreator(unittest.TestCase):
+    def test___repr__(self):
+        # creator = Creator()
+        # self.assertEqual(expected, creator.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # creator = Creator()
+        # self.assertEqual(expected, creator.validate())
+        assert False # TODO: implement your test here
+
+class TestBrowser(unittest.TestCase):
+    def test___repr__(self):
+        # browser = Browser()
+        # self.assertEqual(expected, browser.__repr__())
+        assert False # TODO: implement your test here
+
+class TestPage(unittest.TestCase):
+    def test___repr__(self):
+        # page = Page()
+        # self.assertEqual(expected, page.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # page = Page()
+        # self.assertEqual(expected, page.validate())
+        assert False # TODO: implement your test here
+
+class TestPageTimings(unittest.TestCase):
+    def test___repr__(self):
+        # page_timings = PageTimings()
+        # self.assertEqual(expected, page_timings.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # page_timings = PageTimings()
+        # self.assertEqual(expected, page_timings.validate())
+        assert False # TODO: implement your test here
+
+class TestEntry(unittest.TestCase):
+    def test___repr__(self):
+        # entry = Entry()
+        # self.assertEqual(expected, entry.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # entry = Entry()
+        # self.assertEqual(expected, entry.validate())
+        assert False # TODO: implement your test here
+
+class TestRequest(unittest.TestCase):
+    def test___repr__(self):
+        # request = Request()
+        # self.assertEqual(expected, request.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_devour(self):
+        # request = Request()
+        # self.assertEqual(expected, request.devour(req, proto, comment, keep_b64_raw))
+        assert False # TODO: implement your test here
+
+    def test_puke(self):
+        # request = Request()
+        # self.assertEqual(expected, request.puke())
+        assert False # TODO: implement your test here
+
+    def test_render(self):
+        # request = Request()
+        # self.assertEqual(expected, request.render())
+        assert False # TODO: implement your test here
+
+    def test_set_defaults(self):
+        # request = Request()
+        # self.assertEqual(expected, request.set_defaults())
+        assert False # TODO: implement your test here
+
+    def test_set_header(self):
+        # request = Request()
+        # self.assertEqual(expected, request.set_header(name, value))
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # request = Request()
+        # self.assertEqual(expected, request.validate())
+        assert False # TODO: implement your test here
+
+class TestResponse(unittest.TestCase):
+    def test___repr__(self):
+        # response = Response()
+        # self.assertEqual(expected, response.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_devour(self):
+        # response = Response()
+        # self.assertEqual(expected, response.devour(res, proto, comment, keep_b64_raw))
+        assert False # TODO: implement your test here
+
+    def test_puke(self):
+        # response = Response()
+        # self.assertEqual(expected, response.puke())
+        assert False # TODO: implement your test here
+
+    def test_render(self):
+        # response = Response()
+        # self.assertEqual(expected, response.render())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # response = Response()
+        # self.assertEqual(expected, response.validate())
+        assert False # TODO: implement your test here
+
+class TestCookie(unittest.TestCase):
+    def test___repr__(self):
+        # cookie = Cookie()
+        # self.assertEqual(expected, cookie.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_devour(self):
+        # cookie = Cookie()
+        # self.assertEqual(expected, cookie.devour(cookie_string))
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # cookie = Cookie()
+        # self.assertEqual(expected, cookie.validate())
+        assert False # TODO: implement your test here
+
+class TestPostData(unittest.TestCase):
+    def test_validate(self):
+        # post_data = PostData()
+        # self.assertEqual(expected, post_data.validate())
+        assert False # TODO: implement your test here
+
+class TestParam(unittest.TestCase):
+    def test___repr__(self):
+        # param = Param()
+        # self.assertEqual(expected, param.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # param = Param()
+        # self.assertEqual(expected, param.validate())
+        assert False # TODO: implement your test here
+
+class TestContent(unittest.TestCase):
+    def test___repr__(self):
+        # content = Content()
+        # self.assertEqual(expected, content.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # content = Content()
+        # self.assertEqual(expected, content.validate())
+        assert False # TODO: implement your test here
+
+class TestCache(unittest.TestCase):
+    def test___repr__(self):
+        # cache = Cache()
+        # self.assertEqual(expected, cache.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # cache = Cache()
+        # self.assertEqual(expected, cache.validate())
+        assert False # TODO: implement your test here
+
+class TestRequestCache(unittest.TestCase):
+    def test_validate(self):
+        # request_cache = RequestCache()
+        # self.assertEqual(expected, request_cache.validate())
+        assert False # TODO: implement your test here
+
+class TestTimings(unittest.TestCase):
+    def test___repr__(self):
+        # timings = Timings()
+        # self.assertEqual(expected, timings.__repr__())
+        assert False # TODO: implement your test here
+
+    def test_validate(self):
+        # timings = Timings()
+        # self.assertEqual(expected, timings.validate())
+        assert False # TODO: implement your test here
+
+class TestTest(unittest.TestCase):
+    def test_test(self):
+        # self.assertEqual(expected, test())
+        assert False # TODO: implement your test here
+
+class TestUsage(unittest.TestCase):
+    def test_usage(self):
+        # self.assertEqual(expected, usage())
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
