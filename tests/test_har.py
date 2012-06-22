5fdac5965af0efc7d719a59bcc60418db478cbb7
Josh Dukes <jdukes@securityinnovation.com>
2012-06-22 14:13:49 -0700

    finished tests for TestLog and TestCreator, removed overlapping testcases from HarContainer

--- tests/test_har.py
+++ tests/test_har.py
@@ -70,27 +70,25 @@ class TestHarContainer(unittest.TestCase):
         har_container = har.HarContainer(empty=True)
 
     def test_default_repr__(self):
-        #test defaults
         expected = r"<HarContainer: ('log',)>"
         har_container = har.HarContainer()
         self.assertEqual(expected, har_container.__repr__())
 
     def test_empty_repr__(self):
-        #test empty
         expected = r"<HarContainer: (empty)>"
         har_container = har.HarContainer(empty=True)
         self.assertEqual(expected, har_container.__repr__())
         
     def test_validate_good(self):
-        #make sure good works
+        #make sure good stuff works
         good_json = ('{"log": {"version": "1.2", "creator":'
                      '{"version": "$Id$", "name": "Harpy"}, '
                      '"entries": []}}')
         har_container = har.HarContainer(good_json)
 
     def test_validate_bad_json(self):        
-        #check for invalid json
-        invalid_json = '{"log": {"version": "1.2", "creator": {"version": "$Id$", "name": "Harpy"}, "entries": []}'
+        #check if invalid json fails
+        invalid_json = 'z'
         self.assertRaises(ValueError, har.HarContainer, invalid_json)
         
     def test_validate_missing_values(self):
@@ -102,13 +100,8 @@ class TestHarContainer(unittest.TestCase):
     def test_validate_bad_types(self):
         for bad_types_json in ['{"log": {}}',
                                '{"log":[]}',
-                               ('{"log": {"version": 1.2, "creator":'
-                                '{"version": "$Id$", "name": "Harpy"}, '
-                                '"entries": []}}'),
-                               ('{"log": {"version": 1.2, "creator":'
-                                '[], "entries": []}}'),
-                               ('{"log": {"version": "1.2", "creator":'
-                                '[], "entries": 1}}'),
+                               '{"log":1}',
+                               '{"log":"x"}',
                                ]:
             self.assertRaises(har.ValidationError,
                               har.HarContainer,
@@ -128,16 +121,39 @@ class TestLog(unittest.TestCase):
         log = har.Log()
         self.assertEqual(expected, log.__repr__())
 
-
     def test_empty_repr_(self):
+        expected = "<Log object not fully initilized>"
         log = har.Log(empty=True)
-        # self.assertEqual(expected, log.__repr__())
-        assert False # TODO: implement your test here
+        self.assertEqual(expected, log.__repr__())
 
-    def test_validate(self):
-        # log = har.Log()
-        # self.assertEqual(expected, log.validate())
-        assert False # TODO: implement your test here
+    def test_validate_good(self):
+        #make sure good stuff works
+        good_json = ('{"version": "1.2", "creator":'
+                     '{"version": "$Id$", "name": "Harpy"}, '
+                     '"entries": []}')
+        log = har.Log(good_json)
+
+    def test_validate_bad_json(self):        
+        #check if invalid json fails
+        invalid_json = 'z'
+        self.assertRaises(ValueError, har.Log, invalid_json)
+        
+    def test_validate_missing_values(self):
+        for missing_values_json in ['{}']:
+            self.assertRaises(har.MissingValueException,
+                              har.Log,
+                              missing_values_json)
+
+    def test_validate_bad_types(self):
+        for bad_types_json in [('{"version": 1.2, "creator":'
+                                '{"version": "$Id$", "name": "Harpy"}, '
+                                '"entries": []}'),
+                               '{"version": 1.2, "creator":[], "entries": []}',
+                               ('{"version": "1.2", "creator":[], "entries": 1}'),
+                               ]:
+            self.assertRaises(har.ValidationError,
+                              har.Log,
+                              bad_types_json)
 
 class TestCreator(unittest.TestCase):
 
@@ -157,10 +173,30 @@ class TestCreator(unittest.TestCase):
         creator = har.Creator(empty=True)
         self.assertEqual(expected, creator.__repr__())
 
-    def test_validate(self):
-        # creator = Creator()
-        # self.assertEqual(expected, creator.validate())
-        assert False # TODO: implement your test here
+    def test_validate_good(self):
+        #make sure good stuff works
+        good_json = '{"version": "$Id$", "name": "Harpy"}'
+        log = har.Creator(good_json)
+
+    def test_validate_bad_json(self):        
+        #check if invalid json fails
+        invalid_json = 'z'
+        self.assertRaises(ValueError, har.Creator, invalid_json)
+        
+    def test_validate_missing_values(self):
+        for missing_values_json in ['{}']:
+            self.assertRaises(har.MissingValueException,
+                              har.Creator,
+                              missing_values_json)
+
+    def test_validate_bad_types(self):
+        for bad_types_json in ['{"version": 3, "name": "Harpy"}',
+                               '{"version": "$Id$", "name": 3}'
+                               ]:
+            self.assertRaises(har.ValidationError,
+                              har.Creator,
+                              bad_types_json)
+
 
 class TestBrowser(unittest.TestCase):
     def test___repr__(self):
